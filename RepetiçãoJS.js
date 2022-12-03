aee7a0dcb2bcaceac7cf23b20579fe43dbf02c48
Wagner de Farias Soares <108150341+DevWagnerFarias@users.noreply.github.com>
2022-12-03 15:13:35 -0300

    Update RepetiçãoJS.js

--- RepetiçãoJS.js
+++ RepetiçãoJS.js
@@ -1,3 +1,9 @@
+/*Desenvolva um código e crie nele:
+
+- um objeto com, no mínimo, três propriedades;
+- um array de tamanho três no mínimo;
+- duas funções, a primeira lista as propriedades do objeto e a segunda, os elementos do array*/
+
 let minhaTV = {marca: "Panasonic", tamanha: 39, cor: "Preta"}
 let canais = ["globo", "band", "sbt", "record", "canal brasil"]
 
